c7080c6627dc22ee2d51b4c5896680d450a8fb8a
Kershope Burn <dev.aesc@pm.me>
2024-03-04 13:28:04 +0000

    Compare hash instead of plaintext password

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -1,13 +1,15 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import PasswordForm from './PasswordForm';
-import logo from './logo.svg';
+import bcrypt from 'bcryptjs';
 import './App.css';
 
 function App() {
   const [authenticated, setAuthenticated] = useState(false);
+  const hashedPassword = "$2a$10$7DrCjAlzrc6B3DeiqDFx3unvOcHPi34BDnfp1k7frgfZm/FuZifim"
 
   const handlePasswordSubmit = (password: string) => {
-    if (password === "RjLNr**R%BDb3sfG45ZU") {
+    const isPasswordCorrect = bcrypt.compareSync(password, hashedPassword)
+    if (isPasswordCorrect) {
       setAuthenticated(true);
     } else {
       alert("Incorrect password")
