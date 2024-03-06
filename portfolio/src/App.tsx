1466211ddf55995ac8804fab86d8400f866be8a6
Kershope Burn <dev.aesc@pm.me>
2024-03-06 13:27:14 +0000

    Don't centre Home component

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -13,8 +13,6 @@ function App() {
   const [showPasswordMessage, setShowPasswordMessage] = useState(false);
   const hashedPassword = "$2a$10$7DrCjAlzrc6B3DeiqDFx3unvOcHPi34BDnfp1k7frgfZm/FuZifim"
 
-  const toggleShowPasswordMessage = () => setShowPasswordMessage(!showPasswordMessage)
-
   const handlePasswordSubmit = (password: string) => {
     const isPasswordCorrect = bcrypt.compareSync(password, hashedPassword)
     if (isPasswordCorrect) {
@@ -25,22 +23,25 @@ function App() {
   }
 
   return (
-    <Container className="d-flex justify-content-center align-items-center" style={{ minHeight: '100vh' }}>
-      <Row>
-        <Col>
-          {authenticated ? (
-            <Home />
-          ) : (
-            <>
-              <PasswordForm onSubmit={handlePasswordSubmit} />
-              <Alert variant='warning' show={showPasswordMessage} style={{ marginTop: '20px' }}>
-                Incorrect password
-              </Alert>
-            </>
-          )}
-        </Col>
-      </Row>
-    </Container>
+    <>
+      {authenticated ? (
+        <Home />
+      ) : (
+        // This keeps the password form centred on both axes
+        <Container className="d-flex justify-content-center align-items-center" style={{ minHeight: '100vh' }}>
+          <Row>
+            <Col>
+              <div style={{minHeight: '200px'}}> {/* This stops reflow when the Alert appears */}
+                <PasswordForm onSubmit={handlePasswordSubmit} />
+                <Alert variant='warning' show={showPasswordMessage} style={{ marginTop: '20px' }}>
+                  Incorrect password
+                </Alert>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      )}
+    </>
   )
 }
 
