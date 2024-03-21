5ffd4c946e9ed7e001a231b4e8a19424a37d90cf
Kershope Burn <dev.aesc@pm.me>
2024-03-21 16:05:25 +0000

    Refactor to extract SkillCards

---
+++ portfolio/src/SkillCards.tsx
@@ -0,0 +1,88 @@
+import Badge from 'react-bootstrap/Badge';
+import Card from 'react-bootstrap/Card';
+import CardGroup from 'react-bootstrap/CardGroup';
+import Col from 'react-bootstrap/Col';
+import Stack from 'react-bootstrap/Stack';
+
+interface BadgeProps {
+  content: string;
+  category: string;
+  flex?: boolean;
+}
+
+const SkillBadge: React.FC<BadgeProps> = ({ content, category, flex = true }) => {
+  const categoryToBgColor: { [key: string]: string; } = {
+    language: 'primary',
+    management: 'primary',
+    soft: 'primary',
+    framework: 'info',
+    compliance: 'info',
+    devops: 'success',
+    software: 'dark',
+    other: 'secondary',
+  };
+
+  const categoryLowerCase = category.toLowerCase();
+
+  if (flex) {
+    return (
+      <Col xs={10} md={9} lg={8} className="mx-auto">
+        <Badge bg={categoryToBgColor[categoryLowerCase]} className="w-100">{content}</Badge>
+      </Col>
+    );
+  } else {
+    return (
+      <Badge bg={categoryToBgColor[categoryLowerCase]}>{content}</Badge>
+    );
+  }
+};
+
+const SkillCards = ({ skillBadgesDescriptions }: { skillBadgesDescriptions: Array<{ content: string; category: string; cardTitle?: string; }>; }) => {
+  const cards = [
+    { title: "Advanced", subtitle: "Navigates like a pro" },
+    { title: "Capable", subtitle: "Gets the job done with confidence" },
+    { title: "Developing", subtitle: "Still honing skills, but making strides" },
+    { title: "Rusty but Revivable", subtitle: "Primed for improvement" },
+  ];
+
+  if (skillBadgesDescriptions.some(badge => badge.cardTitle)) {
+    return (
+      <CardGroup>
+        {cards.map((card) => (
+          <Card key={card.title}>
+            <Card.Body>
+              <Card.Title>{card.title}</Card.Title>
+              <Card.Subtitle className="mb-2 text-muted">{card.subtitle}</Card.Subtitle>
+              <Card.Text>
+                <Stack direction="vertical" gap={2} style={{ fontSize: "1.2rem" }}>
+                  {skillBadgesDescriptions
+                    .filter((badge) => badge.cardTitle === card.title)
+                    .map((badge) => (
+                      <SkillBadge content={badge.content} category={badge.category} />
+                    ))}
+                </Stack>
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        ))}
+      </CardGroup>
+    );
+  } else {
+    return (
+      <>
+        <Stack className="justify-content-center" direction="horizontal" gap={2} style={{ padding: '10px', fontSize: "1.3rem" }}>
+          {skillBadgesDescriptions.slice(0, Math.ceil(skillBadgesDescriptions.length / 2)).map((badge) => (
+            <SkillBadge content={badge.content} category={badge.category} flex={false} />
+          ))}
+        </Stack>
+        <Stack className="justify-content-center" direction="horizontal" gap={2} style={{ padding: '10px', fontSize: "1.3rem" }}>
+          {skillBadgesDescriptions.slice(Math.ceil(skillBadgesDescriptions.length / 2)).map((badge) => (
+            <SkillBadge content={badge.content} category={badge.category} flex={false} />
+          ))}
+        </Stack>
+      </>
+    );
+  }
+};
+
+export default SkillCards;
--- portfolio/src/Skills.tsx
+++ portfolio/src/Skills.tsx
@@ -1,7 +1,4 @@
 import { useState } from 'react';
-import Badge from 'react-bootstrap/Badge';
-import Card from 'react-bootstrap/Card';
-import CardGroup from 'react-bootstrap/CardGroup';
 import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
@@ -9,90 +6,10 @@ import Stack from 'react-bootstrap/Stack';
 import Tab from 'react-bootstrap/Tab';
 import Tabs from 'react-bootstrap/Tabs';
 import ToggleButton from 'react-bootstrap/ToggleButton';
+import SkillCards from './SkillCards';
 import { engineeringSkillBadges, managementSkillBadges, softSkillBadges } from './skillBadges';
 
 export function Skills() {
-  interface BadgeProps {
-    content: string;
-    category: string;
-    flex?: boolean;
-  }
-
-  const SkillBadge: React.FC<BadgeProps> = ({ content, category, flex = true }) => {
-    const categoryToBgColor: { [key: string]: string; } = {
-      language: 'primary',
-      management: 'primary',
-      soft: 'primary',
-      framework: 'info',
-      compliance: 'info',
-      devops: 'success',
-      software: 'dark',
-      other: 'secondary',
-    };
-
-    const categoryLowerCase = category.toLowerCase();
-
-    if (flex) {
-      return (
-        <Col xs={10} md={9} lg={8} className="mx-auto">
-          <Badge bg={categoryToBgColor[categoryLowerCase]} className="w-100">{content}</Badge>
-        </Col>
-      );
-    } else {
-      return (
-        <Badge bg={categoryToBgColor[categoryLowerCase]}>{content}</Badge>
-      );
-    }
-  };
-
-  const SkillBadges = ({ skillBadgesDescriptions }: { skillBadgesDescriptions: Array<{ content: string; category: string; cardTitle?: string; }>; }) => {
-    const cards = [
-      { title: "Advanced", subtitle: "Navigates like a pro" },
-      { title: "Capable", subtitle: "Gets the job done with confidence" },
-      { title: "Developing", subtitle: "Still honing skills, but making strides" },
-      { title: "Rusty but Revivable", subtitle: "Primed for improvement" },
-    ];
-
-    if (skillBadgesDescriptions.some(badge => badge.cardTitle)) {
-      return (
-        <CardGroup>
-          {cards.map((card) => (
-            <Card key={card.title}>
-              <Card.Body>
-                <Card.Title>{card.title}</Card.Title>
-                <Card.Subtitle className="mb-2 text-muted">{card.subtitle}</Card.Subtitle>
-                <Card.Text>
-                  <Stack direction="vertical" gap={2} style={{ fontSize: "1.2rem" }}>
-                    {skillBadgesDescriptions
-                      .filter((badge) => badge.cardTitle === card.title)
-                      .map((badge) => (
-                        <SkillBadge content={badge.content} category={badge.category} />
-                      ))}
-                  </Stack>
-                </Card.Text>
-              </Card.Body>
-            </Card>
-          ))}
-        </CardGroup>
-      );
-    } else {
-      return (
-        <>
-          <Stack className="justify-content-center" direction="horizontal" gap={2} style={{ padding: '10px', fontSize: "1.3rem" }}>
-            {skillBadgesDescriptions.slice(0, Math.ceil(skillBadgesDescriptions.length / 2)).map((badge) => (
-              <SkillBadge content={badge.content} category={badge.category} flex={false} />
-            ))}
-          </Stack>
-          <Stack className="justify-content-center" direction="horizontal" gap={2} style={{ padding: '10px', fontSize: "1.3rem" }}>
-            {skillBadgesDescriptions.slice(Math.ceil(skillBadgesDescriptions.length / 2)).map((badge) => (
-              <SkillBadge content={badge.content} category={badge.category} flex={false} />
-            ))}
-          </Stack>
-        </>
-      );
-    }
-  };
-
   type CheckedCategoriesType = {
     [key: string]: boolean;
   };
@@ -208,7 +125,7 @@ export function Skills() {
                   </Col>
                 </Row>
                 <Row>
-                  <SkillBadges skillBadgesDescriptions={filteredEngineeringSkillBadges} />
+                  <SkillCards skillBadgesDescriptions={filteredEngineeringSkillBadges} />
                 </Row>
               </Tab>
               <Tab eventKey="management-skills" title="Management">
@@ -252,12 +169,12 @@ export function Skills() {
                   </Col>
                 </Row>
                 <Row>
-                  <SkillBadges skillBadgesDescriptions={filteredManagementSkillBadges} />
+                  <SkillCards skillBadgesDescriptions={filteredManagementSkillBadges} />
                 </Row>
               </Tab>
               <Tab eventKey="soft-skills" title="Soft">
                 <Row>
-                  <SkillBadges skillBadgesDescriptions={softSkillBadges} />
+                  <SkillCards skillBadgesDescriptions={softSkillBadges} />
                 </Row>
               </Tab>
             </Tabs>
