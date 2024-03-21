245e668943ea9068180c924f905ce390c4a1c711
Kershope Burn <dev.aesc@pm.me>
2024-03-21 10:31:52 +0000

    Have navbar use full width of page

--- portfolio/src/Navigation.tsx
+++ portfolio/src/Navigation.tsx
@@ -20,30 +20,28 @@ function Navigation() {
 
   return (
     <>
-      <Navbar expand="lg" sticky="top" className="bg-transparent" data-bs-theme={getTheme()}>
-        <Container>
-          <Navbar.Brand href="/">ᚫ</Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-              <Nav.Link href="/resume.pdf" target="_blank">Resume</Nav.Link>
-              <Nav.Link as={ReactRouterLink as React.ComponentType<LinkProps>} to="/about">About</Nav.Link>
-              <NavDropdown title="Projects" id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Home Assistant</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">PiHole</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">Internet Performance</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.4">Homes for Ukraine Guide</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.5">Portfolio Site</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.7">DDWRT and Secure IoT</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.8">GrapheneOS</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.9">Ubuntu on Windows Surface Pro</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.6">Charity Site on WordPress</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.10">Gaming Blog on WordPress</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.11">Social Media Automation</NavDropdown.Item>
-              </NavDropdown>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
+      <Navbar style={{ paddingLeft: "1rem", paddingRight: "1rem" }} expand="lg" fixed="top" className="bg-transparent" data-bs-theme={getTheme()}>
+        <Navbar.Brand href="/">ᚫ</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="/resume.pdf" target="_blank">Resume</Nav.Link>
+            <Nav.Link as={ReactRouterLink as React.ComponentType<LinkProps>} to="/about">About</Nav.Link>
+            <NavDropdown title="Projects" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Home Assistant</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">PiHole</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Internet Performance</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.4">Homes for Ukraine Guide</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.5">Portfolio Site</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.7">DDWRT and Secure IoT</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.8">GrapheneOS</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.9">Ubuntu on Windows Surface Pro</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.6">Charity Site on WordPress</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.10">Gaming Blog on WordPress</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.11">Social Media Automation</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
       </Navbar> 
     </>
   );
