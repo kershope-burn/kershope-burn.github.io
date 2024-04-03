b41c8fe50567487d3dcfabc581b2a3d77bfe1b5d
Kershope Burn <dev.aesc@pm.me>
2024-04-03 13:27:59 +0100

    Update ProjectPage layout

--- portfolio/src/ProjectPage.tsx
+++ portfolio/src/ProjectPage.tsx
@@ -4,6 +4,10 @@ import Container from 'react-bootstrap/Container';
 import Image from 'react-bootstrap/Image';
 import ListGroup from 'react-bootstrap/ListGroup';
 import Row from 'react-bootstrap/Row';
+import Tab from 'react-bootstrap/Tab';
+import Tabs from 'react-bootstrap/Tabs';
+
+import './App.css';
 
 function ProjectPage() {
   interface InlineBadgeProps {
@@ -17,20 +21,51 @@ function ProjectPage() {
   return (
     <>
       <div style={{ backgroundColor: 'ghostwhite' }}>
-        <Container className="d-flex flex-column justify-content-center" style={{ minHeight: '60vh' }}>
-          <Row className="align-items-center">
-            <Col>
-              <Row className="text-center" style={{ paddingBottom: '50px' }}>
-                <p className="display-5 portfolio-section-header">Digital security and privacy</p>
-              </Row>
-              <Row className="portfolio-text" style={{ paddingBottom: '20px' }}>
+        <Container className="d-flex flex-column justify-content-center" style={{ minHeight: '70vh' }}>
+          <Row style={{ paddingBottom: '50px' }}>
+            <p className="display-6 portfolio-section-header">projects/digital-security-and-privacy</p>
+          </Row>
+          <Row style={{ minHeight: '40vh' }}>
+            <Col style={{ paddingRight: '50px' }}>
+              <Row className="portfolio-text">
                 <p style={{ fontSize: '1.1rem' }}>Securing information assets and respecting the privacy of all interested parties are aspects of responsible technology. My understanding of how these concepts encroach on personal digital security and privacy - and why I should care about them - deepened by natural proximity when I worked for a cyber security startup.</p>
                 <p style={{ fontSize: '1.1rem' }}>This project isn't "done" in that it is a lifelong attempt to maintain digital security and privacy as technology, my risk profile, and the law evolves. But this page details some of the bigger steps I've taken to protect my digital life.</p>
                 <p style={{ fontSize: '1.1rem' }}>There are some more common steps taken by people who wish to protect their digital security and privacy. VPNs, MFA, password managers, ad-blocking browser extensions, and so on. This page is more concerned with steps that, unfortunately, require more technical nous to implement.</p>
               </Row>
             </Col>
-            <Col xs={6} md={4}>
-              <Image src="/digital-security-and-privacy.png" rounded fluid style={{ padding: '50px' }}/>
+            <Col>
+              <Tabs
+                defaultActiveKey="goals"
+                id="project-summary"
+                className="mb-3 portfolio-title"
+              >
+                <Tab eventKey="goals" title="Goals">
+                  <ListGroup className="portfolio-text">
+                    <ListGroup.Item>Inform and educate myself on personal digital security and privacy and what is reasonably possible to improve these.</ListGroup.Item>
+                    <ListGroup.Item>Improve these aspects by exploring alternative service providers, operating systems, and network hardening.</ListGroup.Item>
+                    <ListGroup.Item>Balance security and privacy with convenience.</ListGroup.Item>
+                  </ListGroup>
+                </Tab>
+                <Tab eventKey="skills" title="Skills">
+                  <ListGroup className="portfolio-text">
+                    <ListGroup.Item>Familiarity with multiple <InlineBadge text="Linux" /> distributions, package managers, the command line, and different hardware running Linux.</ListGroup.Item>
+                    <ListGroup.Item>The finesse to modify operating systems, drivers, and firmware without bricking devices.</ListGroup.Item>
+                    <ListGroup.Item>Use of <InlineBadge text="Docker" /> to run multiple services in parallel. Simple back-up and restore with <InlineBadge text="Docker Compose" />.</ListGroup.Item>
+                    <ListGroup.Item>Comfort with <InlineBadge text="open source software" /> and departing from the beaten track.</ListGroup.Item>
+                    <ListGroup.Item>Experience with home <InlineBadge text="networking" /> that extends into <InlineBadge text="enterprise networking" />. Willingness to experiment and preparing routes to reverse adverse changes.</ListGroup.Item>
+                    <ListGroup.Item>Understanding <InlineBadge text="information security" /> and <InlineBadge text="privacy" /> risks associated with every day activities and engagement with third-parties.</ListGroup.Item>
+                  </ListGroup>
+                </Tab>
+                <Tab eventKey="outcome" title="Outcome">
+                  <ListGroup className="portfolio-text">
+                    <ListGroup.Item>Well-known service providers that rely on extensive data collection have been replaced with trusted (sometimes paid) alternatives.</ListGroup.Item>
+                    <ListGroup.Item>Advertisements have been removed from day-to-day browsing in my home network on all devices and within all applications.</ListGroup.Item>
+                    <ListGroup.Item>The home network has been secured against opportunistic attacks.</ListGroup.Item>
+                    <ListGroup.Item>Requests to smart devices on the home network now stay within the home network through use of open source software.</ListGroup.Item>
+                    <ListGroup.Item>Extensive network hardening and use of open source router firmware proved a step too far in introducing significant inconvenience.</ListGroup.Item>
+                  </ListGroup>
+                </Tab>
+              </Tabs>
             </Col>
           </Row>
         </Container>
@@ -39,20 +74,7 @@ function ProjectPage() {
         <Container className="d-flex flex-column justify-content-center">
           <Row className="align-items-center">
             <Col>
-              <Row className="text-center">
-                <p style={{ fontSize: '1.5rem' }} className="portfolio-title">Demonstrated skills</p>
-              </Row>
-              <Row>
-                <ListGroup className="portfolio-text">
-                  <ListGroup.Item>Familiarity with multiple <InlineBadge text="Linux" /> distributions, package managers, the command line, and different hardware running Linux.</ListGroup.Item>
-                  <ListGroup.Item>The finesse to modify operating systems, drivers, and firmware without bricking devices.</ListGroup.Item>
-                  <ListGroup.Item>Independent working. Identification of a problem, investigation of the solution space, attempt at solutions, and knowing when I've hit a dead-end.</ListGroup.Item>
-                  <ListGroup.Item>Use of <InlineBadge text="Docker" /> to run multiple services in parallel. Simple back-up and restore with <InlineBadge text="Docker Compose" />.</ListGroup.Item>
-                  <ListGroup.Item>Comfort with <InlineBadge text="open source software" /> and departing from the beaten track.</ListGroup.Item>
-                  <ListGroup.Item>Experience with home <InlineBadge text="networking" /> that extends into <InlineBadge text="enterprise networking" />. Willingness to experiment and preparing routes to reverse adverse changes.</ListGroup.Item>
-                  <ListGroup.Item>Understanding <InlineBadge text="information security" /> and <InlineBadge text="privacy" /> risks associated with every day activities and engagement with third-parties.</ListGroup.Item>
-                </ListGroup>
-              </Row>
+              
             </Col>
           </Row>
         </Container>
